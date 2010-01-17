f911f75c9c26d2f54abdb3fef48b45a6dd50ea2b
Esko Luontola <esko.luontola@gmail.com>
2010-01-17 19:35:44 +0200

    Added CrcTest, ObjectSaverTest

---
+++ src/test/java/net/orfjackal/ccorr/CrcTest.java
@@ -0,0 +1,37 @@
+// Copyright © 2003-2010, Esko Luontola <www.orfjackal.net>
+// This software is released under the GNU General Public License, version 2 or later.
+// The license text is at http://www.gnu.org/licenses/gpl.html
+
+package net.orfjackal.ccorr;
+
+import org.junit.*;
+
+/**
+ * @author Esko Luontola
+ */
+public class CrcTest extends Assert {
+
+    private static final byte[] DATA = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    @Test
+    public void calculates_CRC32() {
+        CRC crc32 = new CRC("CRC-32");
+        crc32.update(DATA, 0, DATA.length);
+        assertEquals("456CD746", crc32.getHexValue());
+    }
+
+    @Test
+    public void calculates_MD5() {
+        CRC md5 = new CRC("MD5");
+        md5.update(DATA, 0, DATA.length);
+        assertEquals("c56bd5480f6e5413cb62a0ad9666613a", md5.getHexValue());
+
+    }
+
+    @Test
+    public void calculates_SHA1() {
+        CRC sha1 = new CRC("SHA-1");
+        sha1.update(DATA, 0, DATA.length);
+        assertEquals("494179714a6cd627239dfededf2de9ef994caf03", sha1.getHexValue());
+    }
+}
---
+++ src/test/java/net/orfjackal/ccorr/ObjectSaverTest.java
@@ -0,0 +1,44 @@
+// Copyright © 2003-2010, Esko Luontola <www.orfjackal.net>
+// This software is released under the GNU General Public License, version 2 or later.
+// The license text is at http://www.gnu.org/licenses/gpl.html
+
+package net.orfjackal.ccorr;
+
+import org.junit.*;
+
+import java.io.*;
+
+/**
+ * @author Esko Luontola
+ */
+public class ObjectSaverTest extends Assert {
+
+    private File file;
+
+    @Before
+    public void createFile() throws IOException {
+        file = File.createTempFile(getClass().getName(), null);
+    }
+
+    @After
+    public void deleteFile() {
+        if (!file.delete()) {
+            file.deleteOnExit();
+        }
+    }
+
+    @Test
+    public void loads_and_saves_objects() {
+        Comparison orig = new Comparison();
+        orig.setName("foo");
+
+        ObjectSaver.saveToFile(file, orig);
+
+        assertTrue(file.length() > 0);
+
+        Comparison loaded = (Comparison) ObjectSaver.loadFromFile(file);
+
+        assertEquals(orig.getName(), loaded.getName());
+        assertNotSame(orig, loaded);
+    }
+}
